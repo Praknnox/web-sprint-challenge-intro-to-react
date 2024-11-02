f9f3c1f05476a4538bdb891ddd3ed3745da0168c
Pankorn Sirimanivong <sirimanivongp@gmail.com>
2024-11-01 18:46:35 -0700

    greentext

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Character() { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [worldname,showWorld]=useState(false)
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const toggel=()=>{
+    showWorld(show=>!show)
+  }
   return (
     <div>
       {/* Use the same markup with the same attributes as in the mock */}
